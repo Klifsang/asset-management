dfd29e7f885e2af81daad1473b29eedc06303dbc
Klif <sang.wicklife@student.moringaschool.com>
2024-05-08 22:07:14 +0300

    user login function

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from functools import wraps
+from flask import Flask, jsonify, make_response, redirect, request, url_for
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
@@ -13,10 +14,40 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 from models import admin, assets, requests, employee
+username = 'Klif'
+app.secret_key = 'abc123@assets#manager.com'
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # username = request.cookies.get('username')
+        if username != 'Kliff':
+            # return jsonify({'message': 'This is a protected page'})
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.route('/')
+@login_required
 def home():
-    return '<h1>Asset inventory management</h1>'
+    return jsonify({'username': 'admin', 'password': 'password'})
+
+def login():
+    if request.method == 'POST':
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+        pass
+    return jsonify({'message': 'please login'})
+
+app.add_url_rule('/login', 'login', login, methods=['GET', 'POST'])
+
+@app.route('/logout')
+def logout():
+    resp = make_response(jsonify({'message': 'Logout successful'}), 200)
+    resp.set_cookie('username', '', expires=0)  # Remove the cookie
+    return resp
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- server/migrations/versions/40595a2df427_.py
+++ server/migrations/versions/40595a2df427_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: c04bddf9edcf
+Revision ID: 40595a2df427
 Revises: 
-Create Date: 2024-05-08 02:01:31.562914
+Create Date: 2024-05-08 14:30:20.442881
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'c04bddf9edcf'
+revision = '40595a2df427'
 down_revision = None
 branch_labels = None
 depends_on = None
