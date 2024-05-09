503a1f043bd50011a47cc0992b640a23a4461bdc
Klif <sang.wicklife@student.moringaschool.com>
2024-05-09 03:19:44 +0300

    added bcrypt for hashing passwords

--- server/migrations/versions/6b2abbaa3ae4_.py
+++ server/migrations/versions/6b2abbaa3ae4_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: c5f41303fa29
+Revision ID: 6b2abbaa3ae4
 Revises: 
-Create Date: 2024-05-09 03:08:22.669323
+Create Date: 2024-05-09 03:18:02.684224
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'c5f41303fa29'
+revision = '6b2abbaa3ae4'
 down_revision = None
 branch_labels = None
 depends_on = None
--- server/seed.py
+++ server/seed.py
@@ -1,5 +1,9 @@
 from models import admin, assets, requests, employee
-from app import db, app
+from app import app
+from models.databaseconfig import db
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt(app)
 
 with app.app_context():
     # Assets
@@ -11,11 +15,13 @@ with app.app_context():
     asset1 = assets.Assets(assetname="car",description="Double-cabin",condition="brand new",availability="availabe")
     asset2 = assets.Assets(assetname="Tipper",description="20 tonnes",condition="brand new",availability="availabe")
 
-    admin1 = admin.Admin(email='wiclifsang@gmail.com', password='password', username='klifsang', phonenumber='0123456789', address='112,bomet', role='procurement')
-    admin2 = admin.Admin(email='abel@gmail.com', password='password123', username='Abel', phonenumber='0987654321', address='112,Kapsabet', role='CEO')
+    admin1 = admin.Admin(email='wiclifsang@gmail.com', password=bcrypt.generate_password_hash('password'), username='klifsang', phonenumber='0123456789', address='112,bomet', role='procurement')
+    admin2 = admin.Admin(email='abel@gmail.com', password=bcrypt.generate_password_hash('password123'), username='Abel', phonenumber='0987654321', address='112,Kapsabet', role='CEO')
     
-    employee1 = employee.Employee(username='kelvin@gmail.com', department='Logistics', address='1-Nairobi',email='kelvin@gmail.com',phonenumber='0987654321',password='123password')
-    employee2 = employee.Employee(username='Glen@gmail.com', department='Mechanical', address='100-Nairobi',email='glen@gmail.com',phonenumber='0123456789',password='password123')
+    employee1 = employee.Employee(username='kelvin@gmail.com', department='Logistics', address='1-Nairobi',email='kelvin@gmail.com',phonenumber='0987654321',password=bcrypt.generate_password_hash('123password'))
+    employee2 = employee.Employee(username='Glen@gmail.com', department='Mechanical', address='100-Nairobi',email='glen@gmail.com',phonenumber='0123456789',password=bcrypt.generate_password_hash('password123'))
     
     db.session.add_all([asset1,asset2,admin1, admin2, employee1, employee2])
     db.session.commit()
+    
+    # hashed_password = bcrypt.generate_password_hash(password)
