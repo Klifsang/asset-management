450732c9e28f392ab3d4d8a3683061a0d555b2d4
Klif <sang.wicklife@student.moringaschool.com>
2024-05-13 18:41:58 +0300

    sign up form added, sign in updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,49 @@
-import { useState } from 'react'
-import './App.css'
-import Login from './componenets/Login'
+// App.jsx
+import { useEffect, useState } from "react";
+import "./App.css";
+// import Home from "./components/Home";
+import Login from "./components/Login";
+import SignUp from './components/Signup'
+import HttpClient from "./HttpClient";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loggedIn, setIsLoggedIn] = useState(false);
+  const [showSignUp, setShowSignUp] = useState(true);
+  useEffect(() => {
+    async function checkUser() {
+      const response = await HttpClient.post(
+        "http://127.0.0.1:5000/checksession"
+      );
+      console.log(response.data.id); // Assuming the response contains a 'data' field
+      if (response.data.id) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+    }
+    checkUser();
+  }, []);
 
+    // Function to toggle between SignUp and Login
+    const toggleSignUp = () => {
+      setShowSignUp(!showSignUp);
+    };
+  
   return (
     <>
-      <Login/>
+      {loggedIn ? (
+        <Home setIsLoggedIn={setIsLoggedIn}/>
+      ) : (
+        <>
+          {showSignUp ? (
+            <SignUp toggleSignUp={toggleSignUp} />
+          ) : (
+            <Login toggleSignUp={toggleSignUp} setIsLoggedIn={setIsLoggedIn}/>
+          )}
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/Signup.jsx
@@ -0,0 +1,212 @@
+import { useState } from "react";
+import HttpClient from "../HttpClient";
+
+export default function SignUp({toggleSignUp}){
+    const[username, setUserName] = useState('')
+    const[address, setAddress] = useState('')
+    const[email, setEmail] =useState('')
+    const[phonenumber, setPhonenumber] = useState('')
+    const[password, setPassword] =useState('')
+    const[department, setDepartment] = useState('')
+
+    const handleRegister = async () => {
+        try {
+            const response = await HttpClient.post('http://127.0.0.1:5000/user/register', {
+                username: username,
+                address: address,
+                email: email,
+                phonenumber: phonenumber,
+                department: department,
+                password: password,
+            });
+            console.log(response); // Assuming the response contains a 'data' field
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    
+    return(
+        <>
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img
+              className="mx-auto h-10 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt="Your Company"
+            />
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Create an account
+            </h2>
+          </div>
+  
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-6" action="#" method="POST">
+              <div>
+                <label
+                  htmlFor="fullname"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Full Name
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="fullname"
+                    name="fullname"
+                    type="text"
+                    onChange={(e) => setUserName(e.target.value)}
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div>
+                <label
+                  htmlFor="username"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  User Name
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="username"
+                    name="username"
+                    type="text"
+                    onChange={(e) => setUserName(e.target.value)}
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Email
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="text"
+                    onChange={(e) => setUserName(e.target.value)}
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div>
+                <label
+                  htmlFor="phonenumber"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Phone Number
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="phonenumber"
+                    name="phonenumber"
+                    type="text"
+                    onChange={(e) => setUserName(e.target.value)}
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div>
+                <label
+                  htmlFor="address"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Address
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="address"
+                    name="address"
+                    type="text"
+                    onChange={(e) => setUserName(e.target.value)}
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+  
+              <div>
+                <div className="flex items-center justify-between">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Password
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className="flex items-center justify-between">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Confirm Password
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  onClick={handleRegister}
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Create account
+                </button>
+              </div>
+            </form>
+  
+            <p className="mt-10 text-center text-sm text-gray-500">
+              Already have an account?
+              <a
+                href="#"
+                onClick={toggleSignUp}
+                className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+              >
+                Login
+              </a>
+            </p>
+          </div>
+        </div>
+      </>
+    )
+}
+
+// username = data.get('username')
+// department = data.get('department')
+// address = data.get('address')
+// email = data.get('email')
+// phonenumber = data.get('phonenumber')
+// password = data.get('password')
