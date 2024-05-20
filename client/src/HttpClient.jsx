c5156d8b62c200892e1f3b3cf551968b68c63ac8
Klif <sang.wicklife@student.moringaschool.com>
2024-05-20 18:09:43 +0300

    editing axios settings

--- client/src/HttpClient.jsx
+++ client/src/HttpClient.jsx
@@ -1,8 +1,7 @@
 // httpClient.jsx
 import axios from "axios";
 const HttpClient = axios.create({
-    withCredentials: true,
-    baseURL: "https://asset-management-production.up.railway.app"
+    withCredentials: true
 });
 
 
